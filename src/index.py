1020ce9ff3526bd8ae98da0a93a12d16d56b36b2
sergey-khl <sergeykhlynovskiy@gmail.com>
2023-05-07 23:35:14 -0600

    find json in larger string

--- src/index.py
+++ src/index.py
@@ -1,60 +1,183 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 import json
+import uuid
+from json import JSONDecoder, loads
 
 app = Flask(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 #app.config['CORS_HEADERS'] = 'application/json'
 
-components = ["""{
-"jsx": {
-"type": "TouchableOpacity",
-"uid": "1",
-"props": {
-"activeOpacity": 0.8,
-"onPress": "handleFacebookLogin",
-"style": {
-"backgroundColor": "#3b5998",
-"borderRadius": 10,
-"paddingVertical": 10,
-"paddingHorizontal": 20,
-"flexDirection": "row",
-"alignItems": "center",
-"justifyContent": "center",
-"shadowColor": "#000",
-"shadowOffset": {
-"width": 0,
-"height": 1
-},
-"shadowOpacity": 0.3,
-"shadowRadius": 3,
-"elevation": 5
-}
-},
-"children": [
+
+
+def extract_json_objects(text, decoder=JSONDecoder()):
+    pos = 0
+    while True:
+        match = text.find('{', pos)
+        if match == -1:
+            break
+        try:
+            result, index = decoder.raw_decode(text[match:])
+            return result
+        except ValueError:
+            pos = match + 1
+
+def add_uuid(comp):
+    comp["uuid"] = str(uuid.uuid4())
+    
+    try:
+        for child in comp["children"]:
+            add_uuid(child)
+    except Exception as e:
+        print(e)
+    return comp    
+
+components = r"""Here is an example JSON for the checkout page JSX of a workout application:
+
+```
 {
-"type": "Text",
-"uid": "2",
-"props": {
-"style": {
-"color": "#fff",
-"fontSize": 18,
-"fontWeight": "bold",
-"marginRight": 10
+  "jsx": {
+    "type": "View",
+    "props": {
+      "style": {
+        "flex": 1,
+        "padding": 20
+      }
+    },
+    "children": [
+      {
+        "type": "Text",
+        "props": {
+          "style": {
+            "fontSize": 24,
+            "fontWeight": "bold",
+            "marginBottom": 10
+          }
+        },
+        "children": "Checkout"
+      },
+      {
+        "type": "View",
+        "props": {
+          "style": {
+            "borderWidth": 1,
+            "borderColor": "#ccc",
+            "padding": 10,
+            "marginBottom": 20
+          }
+        },
+        "children": [
+          {
+            "type": "Text",
+            "props": {
+              "style": {
+                "fontSize": 16
+              }
+            },
+            "children": "Total: $50"
+          }
+        ]
+      },
+      {
+        "type": "TextInput",
+        "props": {
+          "style": {
+            "borderWidth": 1,
+            "borderColor": "#ccc",
+            "padding": 10,
+            "marginBottom": 20
+          },
+          "placeholder": "Name on Card"
+        }
+      },
+      {
+        "type": "TextInput",
+        "props": {
+          "style": {
+            "borderWidth": 1,
+            "borderColor": "#ccc",
+            "padding": 10,
+            "marginBottom": 20
+          },
+          "placeholder": "Card Number"
+        }
+      },
+      {
+        "type": "View",
+        "props": {
+          "style": {
+            "flexDirection": "row",
+            "justifyContent": "space-between"
+          }
+        },
+        "children": [
+          {
+            "type": "TextInput",
+            "props": {
+              "style": {
+                "borderWidth": 1,
+                "borderColor": "#ccc",
+                "padding": 10,
+                "width": "48%"
+              },
+              "placeholder": "Expiry Date"
+            }
+          },
+          {
+            "type": "TextInput",
+            "props": {
+              "style": {
+                "borderWidth": 1,
+                "borderColor": "#ccc",
+                "padding": 10,
+                "width": "48%"
+              },
+              "placeholder": "CVV"
+            }
+          }
+        ]
+      },
+      {
+        "type": "TouchableOpacity",
+        "props": {
+          "style": {
+            "backgroundColor": "#4CAF50",
+            "padding": 10,
+            "marginTop": 20,
+            "alignItems": "center"
+          },
+          "onPress": "submit"
+        },
+        "children": [
+          {
+            "type": "Text",
+            "props": {
+              "style": {
+                "color": "#fff",
+                "fontWeight": "bold"
+              }
+            },
+            "children": "Place Order"
+          }
+        ]
+      }
+    ]
+  }
 }
-},
-"children": "Connect with Facebook"
-}
-]
-}
-}"""]
+```
+
+Note that this is just an example and the actual JSX for a checkout page may vary depending on the specific requirements and design of the workout application."""
+
 
 
 
 @app.route('/components', methods=["GET"])
 @cross_origin()
 def get_incomes():
-    return jsonify(json.loads(components[0]))
+    parseString = extract_json_objects(components)
+    component = add_uuid(parseString["jsx"])
+    print(component, flush=True)
+    return jsonify(component)
 
 
 # @app.route('/incomes', methods=['POST'])
